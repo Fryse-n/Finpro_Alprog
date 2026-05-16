2cd0fad41091c4fe2d0e9624a9b5a66e5c742ca5
Fryse-n <faiqrm28@gmail.com>
2026-05-16 20:31:08 +0700

    Create Program.cpp

---
+++ Program.cpp
@@ -0,0 +1,3 @@
+//Todo:
+//3. 📚 Aplikasi Perpustakaan Digital (Library Management)
+//Deskripsi: Sistem peminjaman buku berbasis client-server. Server menyimpan katalog buku, client (mahasiswa) bisa mencari, meminjam, dan mengembalikan buku.
